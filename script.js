42fb0c5fc2dbb0ad1e74cfad6c47b6d3527d18dc
okivlinas <alehkivlinas@gmail.com>
2024-01-23 11:03:42 +0300

    modified script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// Define HTML elements
+const board = document.getElementById('game-board');
+
+// Define game variables
+let snake = [{x: 10, y: 10}];
+
+// Draw game map, snake, food
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+}
+
+// Draw snake
+function drawSnake() {
+     snake.forEach((segment) => {
+        const snakeElement = createGameElement('div', 'snake');
+     });
+}
+
+// Create a snake or food cube/div
+function createGameElement(tag, className){
+    const elent = document.createElement(tag);
+    element.className = className;
+    return element;
+} 
